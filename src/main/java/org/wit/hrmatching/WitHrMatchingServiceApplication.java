af9e3a6a051029cf1671bb33918c8618b896cf9f
fullmoon1219 <ad001219@naver.com>
2025-06-16 18:00:36 +0900

    Initial project upload

---
+++ src/main/java/org/wit/hrmatching/WitHrMatchingServiceApplication.java
@@ -0,0 +1,13 @@
+package org.wit.hrmatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitHrMatchingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WitHrMatchingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/wit/hrmatching/WitHrMatchingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wit.hrmatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitHrMatchingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
