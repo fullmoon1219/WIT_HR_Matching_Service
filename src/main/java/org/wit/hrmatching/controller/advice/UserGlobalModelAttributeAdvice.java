0c7111ec3a2a9f20e1a50a42f2fc5f389ba6b357
fullmoon1219 <ad001219@naver.com>
2025-06-25 13:22:39 +0900

    커뮤니티 초안

---
+++ src/main/java/org/wit/hrmatching/controller/advice/UserGlobalModelAttributeAdvice.java
@@ -0,0 +1,27 @@
+package org.wit.hrmatching.controller.advice;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.wit.hrmatching.controller.community.CommunityController;
+import org.wit.hrmatching.vo.CustomUserDetails;
+import org.wit.hrmatching.vo.UserVO;
+
+//@ControllerAdvice(basePackages = "org.wit.hrmatching.controller.admin")
+@ControllerAdvice(assignableTypes = {CommunityController.class})
+@RequiredArgsConstructor
+public class UserGlobalModelAttributeAdvice {
+
+    @ModelAttribute("user")
+    public UserVO addAdminUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return userDetails != null ? userDetails.getUser() : null;
+    }
+
+    @ModelAttribute("ip")
+    public String addIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        return (ip == null || ip.isEmpty()) ? request.getRemoteAddr() : ip;
+    }
+}
