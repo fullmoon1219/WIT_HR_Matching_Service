3babb1af4b4ecb8666b7fa3349b6aead63e924ed
fullmoon1219 <ad001219@naver.com>
2025-06-25 13:50:57 +0900

    커뮤니티 컨트롤러 수정

--- src/main/java/org/wit/hrmatching/controller/community/CommunityController.java
+++ src/main/java/org/wit/hrmatching/controller/community/CommunityController.java
@@ -1,40 +1,17 @@
 package org.wit.hrmatching.controller.community;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/community")
 public class CommunityController {
 
-    @GetMapping("/all")
-    public String all() {
-        return "community/all";
-    }
-
-    @GetMapping("/free")
-    public String free() {
-        return "community/free";
-    }
-
-    @GetMapping("/qna")
-    public String qna() {
-        return "community/qna";
-    }
-
-    @GetMapping("/study")
-    public String study() {
-        return "community/study";
-    }
-
-    @GetMapping("/feedback")
-    public String feedback() {
-        return "community/feedback";
-    }
-
-    @GetMapping("/job-share")
-    public String jobShare() {
-        return "community/job-share";
+    @GetMapping("/{boardCode}")
+    public String showBoardList(@PathVariable String boardCode) {
+        return "community/board-list";
     }
 }
