dae365fa49acc2773eeb5bf6dd6b7e5004ab7962
fullmoon1219 <ad001219@naver.com>
2025-06-25 16:26:46 +0900

    커뮤니티 게시판 목록, 글쓰기, 보기 html 추가

--- src/main/java/org/wit/hrmatching/controller/community/CommunityController.java
+++ src/main/java/org/wit/hrmatching/controller/community/CommunityController.java
@@ -1,17 +1,46 @@
 package org.wit.hrmatching.controller.community;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.wit.hrmatching.service.community.BoardService;
+import org.wit.hrmatching.vo.community.BoardVO;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/community")
 public class CommunityController {
+
+    private final BoardService boardService;
 
     @GetMapping("/{boardCode}")
     public String showBoardList(@PathVariable String boardCode) {
         return "community/board-list";
     }
+
+    @GetMapping("/{boardCode}/write")
+    public String showWritePage(@PathVariable String boardCode, Model model) {
+        if ("all".equals(boardCode)) {
+            boardCode = "free";
+        }
+        model.addAttribute("boardCode", boardCode);
+
+
+        List<BoardVO> boardList = boardService.findAllBoards();
+        model.addAttribute("boardList", boardList);
+
+        return "community/board-write";
+    }
+
+    @GetMapping("/posts/view/{id}")
+    public String getPostFragment(@PathVariable Long id) {
+
+        return "community/board-view";
+    }
+
 }
